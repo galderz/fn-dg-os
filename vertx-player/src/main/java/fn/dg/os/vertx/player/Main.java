31708b1eb3a89e244cab3092374cae4e014adbed
Galder Zamarreño <galder@zamarreno.com>
2018-03-15 18:47:41 +0100

    Add Player leaderboard prototype

---
+++ vertx-player/src/main/java/fn/dg/os/vertx/player/Main.java
@@ -0,0 +1,143 @@
+package fn.dg.os.vertx.player;
+
+import io.netty.handler.logging.LogLevel;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.Future;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.Search;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
+import org.infinispan.protostream.SerializationContext;
+import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
+import org.infinispan.query.dsl.Query;
+import org.infinispan.query.dsl.QueryFactory;
+import org.infinispan.query.dsl.SortOrder;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.infinispan.query.dsl.Expression.max;
+
+public class Main extends AbstractVerticle {
+
+   static final Logger log = Logger.getLogger(Main.class.getName());
+
+   @Override
+   public void start(io.vertx.core.Future<Void> future) {
+      Router router = Router.router(vertx);
+      router.get("/inject").handler(this::inject);
+      router.get("/leaderboard").handler(this::getLeaderboard);
+
+      vertx
+         .createHttpServer()
+         .requestHandler(router::accept)
+         .rxListen(8080)
+         .subscribe(
+            server -> {
+               log.info("Http server started");
+               future.complete();
+            }
+            , future::fail
+         );
+   }
+
+   private void inject(RoutingContext rc) {
+      vertx
+         .rxExecuteBlocking(Main::remoteCacheManager)
+         .flatMap(remote -> vertx.rxExecuteBlocking(remoteCache(remote)))
+         .subscribe(
+            cache -> {
+               Random r = new Random();
+
+               vertx.setPeriodic(1000, id -> {
+                  final String uuid = UUID.randomUUID().toString();
+                  int score = r.nextInt(1000) + 100; // 3 digit number
+                  cache.putAsync(uuid, new Player(uuid, score));
+               });
+
+               rc.response().end("Injector started");
+            }
+            , failure ->
+               rc.response().end("Failed: " + failure)
+         );
+   }
+
+   private void getLeaderboard(RoutingContext rc) {
+      vertx
+         .rxExecuteBlocking(Main::remoteCacheManager)
+         .flatMap(remote -> vertx.rxExecuteBlocking(remoteCache(remote)))
+         .flatMap(cache -> vertx.rxExecuteBlocking(leaderboard(cache)))
+         .subscribe(
+            json ->
+               rc.response().end(json.toString())
+            , failure ->
+               rc.response().end("Failed: " + failure)
+         );
+   }
+
+   private static Handler<Future<JsonObject>> leaderboard(RemoteCache<String, Player> remoteCache) {
+      return f -> f.complete(queryLeaderboard(remoteCache));
+   }
+
+   private static JsonObject queryLeaderboard(RemoteCache<String, Player> remoteCache) {
+      QueryFactory qf = Search.getQueryFactory(remoteCache);
+      Query query = qf.from(Player.class)
+         .orderBy("score")
+         .maxResults(10)
+         .build();
+      List<Player> list = query.list();
+      JsonObject json = new JsonObject();
+      list.forEach(player ->
+         json.put(player.getId().toString(), player.getScore())
+      );
+
+      return json;
+   }
+
+   private static void remoteCacheManager(Future<RemoteCacheManager> f) {
+      final RemoteCacheManager remote = new RemoteCacheManager(
+         new ConfigurationBuilder()
+            .addServer()
+               //.host("jdg-app-hotrod")
+               .host("infinispan-app-hotrod")
+               .port(11222)
+            .marshaller(ProtoStreamMarshaller.class)
+            .build()
+      );
+
+      SerializationContext serialCtx =
+         ProtoStreamMarshaller.getSerializationContext(remote);
+
+      ProtoSchemaBuilder protoSchemaBuilder = new ProtoSchemaBuilder();
+      try {
+         String playerSchemaFile = protoSchemaBuilder.fileName("player.proto")
+            .addClass(Player.class)
+            .build(serialCtx);
+
+         RemoteCache<String, String> metadataCache = remote
+            .getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
+
+         metadataCache.put("player.proto", playerSchemaFile);
+
+         f.complete(remote);
+      } catch (IOException e) {
+         log.log(Level.SEVERE, "Unable to auto-generate player.proto", e);
+         f.fail(e);
+      }
+   }
+
+   private static Handler<Future<RemoteCache<String, Player>>> remoteCache(RemoteCacheManager remote) {
+      return f -> f.complete(remote.getCache("index"));
+   }
+
+}
---
+++ vertx-player/src/main/java/fn/dg/os/vertx/player/Player.java
@@ -0,0 +1,45 @@
+package fn.dg.os.vertx.player;
+
+import org.infinispan.protostream.annotations.ProtoDoc;
+import org.infinispan.protostream.annotations.ProtoField;
+import org.infinispan.protostream.annotations.ProtoMessage;
+
+import java.util.UUID;
+
+@ProtoDoc("@Indexed")
+@ProtoMessage(name = "Player")
+public class Player {
+
+   private String id;
+   private int score;
+
+   // Required for proto schema builder
+   public Player() {
+   }
+
+   public Player(String id, int score) {
+      this.score = score;
+      this.id = id;
+   }
+
+   @ProtoDoc("@Field(index = Index.NO, store = Store.NO)")
+   @ProtoField(number = 10, required = true)
+   public String getId() {
+      return id;
+   }
+
+   public void setId(String id) {
+      this.id = id;
+   }
+
+   @ProtoDoc("@IndexedField")
+   @ProtoField(number = 20, required = true)
+   public int getScore() {
+      return score;
+   }
+
+   public void setScore(int score) {
+      this.score = score;
+   }
+
+}
