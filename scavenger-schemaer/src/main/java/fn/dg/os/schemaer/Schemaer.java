4600cfcf145bc184501b144ade5935d14fbf1f1b
Galder Zamarreño <galder@zamarreno.com>
2018-04-19 17:56:18 +0200

    Update schemaer to use progressive timer checks
    * Also include creating RemoteCacheManager inside the timer handler.

--- scavenger-schemaer/src/main/java/fn/dg/os/schemaer/Schemaer.java
+++ scavenger-schemaer/src/main/java/fn/dg/os/schemaer/Schemaer.java
@@ -10,12 +10,14 @@ import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
 import org.infinispan.protostream.SerializationContext;
 import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
-import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static org.infinispan.query.remote.client.ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME;
 
 public class Schemaer extends AbstractVerticle {
 
@@ -27,12 +29,16 @@ public class Schemaer extends AbstractVerticle {
     RemoteCacheManager remote;
     long timerId;
 
+    AtomicInteger counter;
+
     @Override
     public void start(io.vertx.core.Future<Void> future) {
         hotrod = System.getenv("HOTROD_SERVICE_NAME");
         String delayEnv = System.getenv("SCHEMA_CHECK_DELAY");
         if (Objects.isNull(hotrod) || Objects.isNull(delayEnv))
-            future.fail("HOTROD_SERVICE_NAME and SCHEMA_CHECK_DELAY env variables need to be given");
+            future.fail(
+                "HOTROD_SERVICE_NAME and SCHEMA_CHECK_DELAY env variables need to be given"
+            );
         
         delay = Integer.parseInt(delayEnv);
 
@@ -55,7 +61,9 @@ public class Schemaer extends AbstractVerticle {
             .rxExecuteBlocking(stopRemote(remote))
             .subscribe(
                 server -> {
-                    log.info("Timer cancelled and remote connection stopped");
+                    log.info(
+                        "Timer cancelled and remote connection stopped"
+                    );
                     future.complete();
                 }
                 , future::fail
@@ -64,44 +72,61 @@ public class Schemaer extends AbstractVerticle {
 
     private Handler<Future<Void>> stopRemote(RemoteCacheManager remote) {
         return f -> {
-            remote.stop();
+            if (remote != null)
+                remote.stop();
             f.complete();
         };
     }
 
     private void startSchemaer(Future<Void> future) {
-        this.remote = new RemoteCacheManager(
-            new ConfigurationBuilder()
-                .addServer()
-                    .host(hotrod)
-                    .port(11222)
-                .marshaller(ProtoStreamMarshaller.class)
-                .build()
-        );
+        timerId = vertx.setTimer(getDelay(), id -> {
+            tryAddSchema();
 
-        timerId = vertx.setPeriodic(delay, id -> {
-            RemoteCache<String, String> metadataCache = remote
-                .getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
-
-            final String metadata = metadataCache.get("player.proto");
-            if (metadata == null)
-                registerSchema(metadataCache);
-            else
-                log.info(String.format("Player schema already registered in service=%s", hotrod));
+            vertx.setTimer(getDelay(), x -> tryAddSchema());
         });
 
         future.complete();
     }
 
+    private void tryAddSchema() {
+        try {
+            this.remote = new RemoteCacheManager(
+                new ConfigurationBuilder()
+                    .addServer()
+                    .host(hotrod)
+                    .port(11222)
+                    .marshaller(ProtoStreamMarshaller.class)
+                    .build()
+            );
+
+            RemoteCache<String, String> metadataCache = remote
+                .getCache(PROTOBUF_METADATA_CACHE_NAME);
+
+            final String metadata = metadataCache.get("player.proto");
+            if (metadata == null)
+                registerSchema(metadataCache);
+            else
+                log.info(String.format(
+                    "Player schema already registered in service=%s", hotrod
+                ));
+        } catch (Throwable t) {
+            log.log(Level.WARNING, "Schemaer failed", t);
+            counter.set(0);
+        }
+    }
+
     private void registerSchema(RemoteCache<String, String> metaCache) {
-        log.info(String.format("Register Player schema in service=%s", hotrod));
+        log.info(String.format(
+            "Register Player schema in service=%s", hotrod
+        ));
 
         SerializationContext serialCtx =
             ProtoStreamMarshaller.getSerializationContext(remote);
 
         ProtoSchemaBuilder protoSchemaBuilder = new ProtoSchemaBuilder();
         try {
-            String playerSchemaFile = protoSchemaBuilder.fileName("player.proto")
+            String playerSchemaFile = protoSchemaBuilder
+                .fileName("player.proto")
                 .addClass(Player.class)
                 .build(serialCtx);
 
@@ -111,8 +136,25 @@ public class Schemaer extends AbstractVerticle {
             if (errors != null)
                 log.severe("Errors found in proto file: " + errors);
         } catch (IOException e) {
-            log.log(Level.SEVERE, "Unable to auto-generate player.proto", e);
+            log.log(
+                Level.SEVERE
+                , "Unable to auto-generate player.proto"
+                , e
+            );
         }
+    }
+
+    // milliseconds
+    private int getDelay() {
+        final int count = this.counter.get();
+        if (count < 10)
+            return 5_000;
+        else if (count < 20)
+            return 10_000;
+        else if (count < 30)
+            return 30_000;
+
+        return 60_000;
     }
 
 }
